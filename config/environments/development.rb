85dcf71f34eb28e2f9ac5d4ece394bf793b1fc99
Mike Hoitomt <mhoitomt@lookingglasscyber.com>
2020-09-16 21:06:48 -0500

    Switch back to MailGun. Cross Fingers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,11 +36,11 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.show_previews = true
   config.action_mailer.smtp_settings = {
-    :port           => ENV['SENDGRID_SMTP_PORT'],
-    :address        => ENV['SENDGRID_SMTP_SERVER'],
-    :user_name      => ENV['SENDGRID_SMTP_LOGIN'],
-    :password       => ENV['SENDGRID_SMTP_PASSWORD'],
-    :domain         => 'ecshamrocksbasketball.com',
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'shamrocks.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,11 +65,11 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.smtp_settings = {
-    :port           => ENV['SENDGRID_SMTP_PORT'].to_i,
-    :address        => ENV['SENDGRID_SMTP_SERVER'],
-    :user_name      => ENV['SENDGRID_SMTP_LOGIN'],
-    :password       => ENV['SENDGRID_SMTP_PASSWORD'],
-    :domain         => 'ecshamrocksbasketball.com',
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'shamrocks.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
